cd6593fee9a4200aa453dc37e030a6f924f2c390
Odilson Woodly Italis <italisodilsonwoodly@hotmail.com>
2024-07-11 16:04:16 -0400

    Add routes and views for the form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Message Board", messages: messages });
+});
+
+router.get("/new", (req, res, next) => {
+  res.render("form");
+});
+
+router.post("/new", (req, res, next) => {
+  messages.push({
+    text: req.body.message,
+    user: req.body.author,
+    added: new Date(),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
